94861b470c7c8a91bef09118e06ba1e6300ea22d
Eugen Grosu <Eugen Grosu>
2016-01-30 18:21:50 +0100

    all alg

--- supervised/src/main/java/com/gt/ml/ClassifierBuilder.java
+++ supervised/src/main/java/com/gt/ml/ClassifierBuilder.java
@@ -2,6 +2,10 @@ package com.gt.ml;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.functions.SMO;
+import weka.classifiers.functions.supportVector.Kernel;
+import weka.classifiers.meta.AdaBoostM1;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
 
@@ -12,6 +16,26 @@ public class ClassifierBuilder {
 		dt.setUnpruned(!pruning);
 		dt.buildClassifier(trainingSet);
 		return dt;
+	}
+	
+	public AdaBoostM1 runBoosting(Instances trainingSet, Classifier classifier) throws Exception {
+		AdaBoostM1 boost = new AdaBoostM1();
+		boost.setClassifier(classifier);
+		boost.buildClassifier(trainingSet);
+		return boost;
+	}
+	
+	public Classifier runNeuralNet(Instances trainingSet) throws Exception {
+		MultilayerPerceptron nn = new MultilayerPerceptron();
+		nn.buildClassifier(trainingSet);
+		return nn;
+	}
+	
+	public Classifier runSvm(Instances trainingSet, Kernel kernel) throws Exception {
+		SMO svm = new SMO();
+		svm.setKernel(kernel);
+		svm.buildClassifier(trainingSet);
+		return svm;
 	}
 	
 	public Evaluation evaluate(Classifier classifier, Instances testSet) throws Exception {
--- supervised/src/main/java/com/gt/ml/RunAssignment.java
+++ supervised/src/main/java/com/gt/ml/RunAssignment.java
@@ -6,15 +6,17 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Enumeration;
 
-import weka.core.*;
-
+import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.functions.supportVector.Kernel;
+import weka.classifiers.functions.supportVector.PolyKernel;
+import weka.classifiers.trees.DecisionStump;
 import weka.classifiers.trees.J48;
+import weka.core.Attribute;
 import weka.core.Instances;
 
 public class RunAssignment {
 	
-	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception {
 		if (args.length != 1) {
 			System.out.println("Please provide file name");
@@ -32,9 +34,46 @@ public class RunAssignment {
 		Instances trainingSet = new Instances(dataSet, 0, trainingSize);
 		Instances testSet = new Instances(dataSet, trainingSize, testSize);
 		
+		System.out.println("------------ DECISION TREE -------------");
 		ClassifierBuilder classifierBuilder = new ClassifierBuilder();
-		J48 dt = classifierBuilder.runDecissionTree(trainingSet, true);
-		Evaluation eval = classifierBuilder.evaluate(dt, testSet);
+		Classifier classifier = classifierBuilder.runDecissionTree(trainingSet, true);
+		Evaluation eval = classifierBuilder.evaluate(classifier, testSet);
+		printStats(eval, trainingSet, testSet, classifier);
+		
+		System.out.println("--------------BOOSTING WITH STUMP -------");
+		
+		classifierBuilder = new ClassifierBuilder();
+		classifier = classifierBuilder.runBoosting(trainingSet, new DecisionStump());
+		eval = classifierBuilder.evaluate(classifier, testSet);
+		printStats(eval, trainingSet, testSet, classifier);
+		
+		System.out.println("--------------BOOSTING WITH J48 -------");
+		
+		classifierBuilder = new ClassifierBuilder();
+		classifier = classifierBuilder.runBoosting(trainingSet, new J48());
+		eval = classifierBuilder.evaluate(classifier, testSet);
+		printStats(eval, trainingSet, testSet, classifier);
+		
+		System.out.println("-------------- NEURAL NET --------------");
+		
+		classifierBuilder = new ClassifierBuilder();
+		classifier = classifierBuilder.runNeuralNet(trainingSet);
+		eval = classifierBuilder.evaluate(classifier, testSet);
+		printStats(eval, trainingSet, testSet, classifier);
+		
+		System.out.println("-------------- SVM ---------------------");
+		
+		classifierBuilder = new ClassifierBuilder();
+		PolyKernel kernel = new PolyKernel();
+		kernel.setExponent(2);
+		classifier = classifierBuilder.runSvm(trainingSet, kernel);
+		eval = classifierBuilder.evaluate(classifier, testSet);
+		printStats(eval, trainingSet, testSet, classifier);
+	}
+
+	@SuppressWarnings("unchecked")
+	private static void printStats(Evaluation eval, Instances trainingSet, Instances testSet, Classifier classifier)
+			throws Exception {
 		System.out.println(eval.toMatrixString());
 		System.out.println("accuracy: " + eval.pctCorrect());
 		Attribute output = trainingSet.attribute(36);
@@ -42,7 +81,8 @@ public class RunAssignment {
 		int index = 0;
 		while (values.hasMoreElements()) {
 			String value = values.nextElement();
-			System.out.println(value + " -> " + eval.precision(index) + "; " + eval.recall(index));
+			System.out.println(
+					value + " -> " + eval.precision(index) + "; " + eval.recall(index) + "; " + eval.fMeasure(index));
 			index++;
 		}
 	}

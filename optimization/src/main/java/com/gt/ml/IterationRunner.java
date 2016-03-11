2e66f29c1576c203552932cfd16a72afaeb0ed48
eugen <eugen.grosu@veeva.com>
2016-03-12 00:08:45 +0100

    new pb runner with options

---
+++ optimization/src/main/java/com/gt/ml/IterationRunner.java
@@ -0,0 +1,136 @@
+package com.gt.ml;
+
+import java.util.Arrays;
+
+import dist.DiscreteDependencyTree;
+import dist.DiscreteUniformDistribution;
+import dist.Distribution;
+import opt.DiscreteChangeOneNeighbor;
+import opt.EvaluationFunction;
+import opt.GenericHillClimbingProblem;
+import opt.HillClimbingProblem;
+import opt.NeighborFunction;
+import opt.RandomizedHillClimbing;
+import opt.SimulatedAnnealing;
+import opt.ga.CrossoverFunction;
+import opt.ga.DiscreteChangeOneMutation;
+import opt.ga.GenericGeneticAlgorithmProblem;
+import opt.ga.GeneticAlgorithmProblem;
+import opt.ga.MutationFunction;
+import opt.ga.StandardGeneticAlgorithm;
+import opt.ga.UniformCrossOver;
+import opt.prob.GenericProbabilisticOptimizationProblem;
+import opt.prob.MIMIC;
+import opt.prob.ProbabilisticOptimizationProblem;
+import shared.FixedIterationTrainer;
+import shared.Instance;
+
+public class IterationRunner {
+	
+	private int runs;
+	private int N;
+	private int iterations;
+	
+	private EvaluationFunction ef;
+	
+	public IterationRunner runs(int value) {
+		this.runs = value;
+		return this;
+	}
+	
+	public IterationRunner n(int value) {
+		this.N = value;
+		return this;
+	}
+	
+	public IterationRunner iterations(int value) {
+		this.iterations = value;
+		return this;
+	}
+	
+	public IterationRunner fct(EvaluationFunction value) {
+		this.ef = value;
+		return this;
+	}
+	
+	public void runAll() {
+		runRHC();
+		runSA();
+		runGA();
+		runMimic();
+	}
+	
+	public void runRHC() {
+		System.out.println("##### RHC start");
+		for (int r = 0; r < runs; r++) {
+			int[] ranges = new int[N];
+	        Arrays.fill(ranges, 2);        
+	        Distribution odd = new DiscreteUniformDistribution(ranges);
+	        NeighborFunction nf = new DiscreteChangeOneNeighbor(ranges);
+	        HillClimbingProblem hcp = new GenericHillClimbingProblem(ef, odd, nf);
+	        RandomizedHillClimbing rhc = new RandomizedHillClimbing(hcp);
+	        FixedIterationTrainer fit = new FixedIterationTrainer(rhc, iterations);
+	        fit.train();
+	        Instance optimal = rhc.getOptimal();
+	        double optimalValue = ef.value(optimal);
+	        System.out.println("RHC," + optimalValue);
+		}
+		System.out.println("##### RHC end");
+	}
+	
+	public void runSA() {
+		System.out.println("##### SA start");
+		for (int r = 0; r < runs; r++) {
+			int[] ranges = new int[N];
+	        Arrays.fill(ranges, 2);        
+	        Distribution odd = new DiscreteUniformDistribution(ranges);
+	        NeighborFunction nf = new DiscreteChangeOneNeighbor(ranges);
+	        HillClimbingProblem hcp = new GenericHillClimbingProblem(ef, odd, nf);
+	        SimulatedAnnealing sa = new SimulatedAnnealing(100, .95, hcp);	        
+	        FixedIterationTrainer fit = new FixedIterationTrainer(sa, iterations);
+	        fit.train();
+	        Instance optimal = sa.getOptimal();
+	        double optimalValue = ef.value(optimal);
+			System.out.println("SA," + optimalValue);
+		}
+		System.out.println("##### SA end");
+	}
+	
+	public void runGA() {
+		System.out.println("##### GA start");
+		for (int r = 0; r < runs; r++) {
+			int[] ranges = new int[N];
+	        Arrays.fill(ranges, 2);        
+	        Distribution odd = new DiscreteUniformDistribution(ranges);
+	        MutationFunction mf = new DiscreteChangeOneMutation(ranges);
+	        CrossoverFunction cf = new UniformCrossOver();	        
+	        GeneticAlgorithmProblem gap = new GenericGeneticAlgorithmProblem(ef, odd, mf, cf);			
+	        StandardGeneticAlgorithm ga = new StandardGeneticAlgorithm(20, 20, 0, gap);	        
+	        FixedIterationTrainer fit = new FixedIterationTrainer(ga, iterations);
+	        fit.train();
+	        Instance optimal = ga.getOptimal();
+	        double optimalValue = ef.value(optimal);
+			System.out.println("GA," + optimalValue);
+		}
+		System.out.println("##### GA end");
+	}
+	
+	public void runMimic() {
+		System.out.println("##### MIMIC start");
+		for (int r = 0; r < runs; r++) {
+			int[] ranges = new int[N];
+	        Arrays.fill(ranges, 2);        
+	        Distribution odd = new DiscreteUniformDistribution(ranges);
+	        Distribution df = new DiscreteDependencyTree(.1, ranges);
+	        ProbabilisticOptimizationProblem pop = new GenericProbabilisticOptimizationProblem(ef, odd, df);
+	        MIMIC mimic = new MIMIC(50, 10, pop);	        
+	        FixedIterationTrainer fit = new FixedIterationTrainer(mimic, iterations);
+	        fit.train();
+	        Instance optimal = mimic.getOptimal();
+	        double optimalValue = ef.value(optimal);
+			System.out.println("MIMIC," + optimalValue);
+		}
+		System.out.println("##### MIMIC end");
+	}
+
+}
---
+++ optimization/src/main/java/com/gt/ml/PbRunner.java
@@ -0,0 +1,134 @@
+package com.gt.ml;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import com.gt.ml.evenOdd.Even0Odd1EvaluationFunction;
+
+import opt.EvaluationFunction;
+import opt.example.CountOnesEvaluationFunction;
+/*
+optimization.bat -p 1 -a all -N 100 -r 3 -i 10
+ */
+public class PbRunner {
+	
+	public static void main(String[] args) throws ParseException {		
+		Options options = new Options()
+				.addOption(new Option("h", "help", false, "show help"))
+				.addOption(new Option("p", "problem", true, "type of problem, one of <1=countOnes,2=Even0Odd1>"))
+				.addOption(new Option("a", "alg", true, "algorithm to run, one of <all,rhc,sa,ga,mimic>"))
+				.addOption(new Option("N", "N", true, "size of problem"))
+				.addOption(new Option("r", "restarts", true, "#restarts (runs)"))
+				.addOption(new Option("i", "iterations", true, "#iterations per each run"))
+				.addOption(new Option("t", "time", true, "time in millis or sec; e.g. 100m=100 millis, 10=10seconds"));
+		
+		CommandLine commandLine = new DefaultParser().parse(options, args);
+		if (commandLine.hasOption("h")) {
+			new HelpFormatter().printHelp("Run Problem", options);
+			return;
+		}
+		
+		int problem;
+		String alg;
+		int N;
+		int runs;
+		int iterations = -1;
+		long timeMillis = -1;
+		
+		if (commandLine.hasOption("p")) {
+			problem = Integer.valueOf(commandLine.getOptionValue("p"));
+		} else {
+			System.out.println("Please choose a problem; see help for information");
+			return;
+		}
+		
+		if (commandLine.hasOption("a")) {
+			alg = commandLine.getOptionValue("a");
+		} else {
+			System.out.println("Please choose an algorithm; see help for information");
+			return;
+		}
+		
+		if (commandLine.hasOption("N")) {
+			N = Integer.valueOf(commandLine.getOptionValue("N"));
+		} else {
+			System.out.println("Please choose problem size; see help for information");
+			return;
+		}
+		
+		if (commandLine.hasOption("r")) {
+			runs = Integer.valueOf(commandLine.getOptionValue("r"));
+		} else {
+			System.out.println("Please choose #runs; see help for information");
+			return;
+		}
+		
+		if (!commandLine.hasOption("i") && !commandLine.hasOption("t")) {
+			System.out.println("Please choose whether using iterations or time; see help for information");
+			return;
+		} 
+		
+		boolean it = false;
+		if (commandLine.hasOption("i")) {
+			iterations = Integer.valueOf(commandLine.getOptionValue("i"));
+			it = true;
+		} else {
+			String time = commandLine.getOptionValue("t");
+			if (time.endsWith("m")) {
+				timeMillis = Long.valueOf(time.substring(0, time.length()-1));
+			} else {
+				timeMillis = Long.valueOf(time) * 1000;
+			}
+		}
+		
+		EvaluationFunction fct = getFct(problem);
+		if (it) {
+			System.out.println("Running Iteration pb " + fct.getClass().getSimpleName());	
+		} else {
+			System.out.println("Running Time pb " + fct.getClass().getSimpleName());
+		}
+		
+		
+		if (it) {
+			IterationRunner runner = new IterationRunner().n(N).runs(runs).iterations(iterations).fct(fct);
+			if (alg.equals("all")) {
+				runner.runAll();
+			} else if (alg.equals("rhc")) {
+				runner.runRHC();
+			} else if (alg.equals("sa")) {
+				runner.runSA();
+			} else if (alg.equals("ga")) {
+				runner.runGA();
+			} else if (alg.equals("mimic")) {
+				runner.runMimic();
+			}
+		} else {
+			TimeRunner runner = new TimeRunner().n(N).runs(runs).time(timeMillis).fct(fct);
+			if (alg.equals("all")) {
+				runner.runAll();
+			} else if (alg.equals("rhc")) {
+				runner.runRHC();
+			} else if (alg.equals("sa")) {
+				runner.runSA();
+			} else if (alg.equals("ga")) {
+				runner.runGA();
+			} else if (alg.equals("mimic")) {
+				runner.runMimic();
+			}
+		}
+    }
+
+	private static EvaluationFunction getFct(int problem) {
+		if (problem == 1) {
+			return new CountOnesEvaluationFunction();
+		} else if (problem == 2) {
+			return new Even0Odd1EvaluationFunction();
+		} 
+		throw new RuntimeException("type of problem, one of <1=countOnes,2=Even0Odd1>");
+	}
+
+}

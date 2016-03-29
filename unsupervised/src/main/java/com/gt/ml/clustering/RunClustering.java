eb526102c501425bdb4e6dbda25fbad1d35dea94
Eugen Grosu <grosueugen@gmail.com>
2016-03-29 23:58:16 +0200

    unsupervised: starting clustering

--- unsupervised/src/main/java/com/gt/ml/clustering/RunClustering.java
+++ unsupervised/src/main/java/com/gt/ml/clustering/RunClustering.java
@@ -1,5 +1,40 @@
 package com.gt.ml.clustering;
 
-public class RunClustering {
+import weka.clusterers.ClusterEvaluation;
+import weka.clusterers.SimpleKMeans;
 
+public class RunClustering {
+	
+	public static void main(String[] args) throws Exception {
+		//runKMeansSat();
+		runKMeansWine();
+	}
+
+	// params: file: sat, wine, distance
+	private static void runKMeansSat() throws Exception {
+		SimpleKMeans km = new SimpleKMeans();
+		km.setNumClusters(6);
+		
+		String res = ClusterEvaluation.evaluateClusterer(km, 
+				new String[]{
+						"-t", "src/main/resources/sat.arff", 
+						"-c", "last",
+						"-A", "weka.core.ManhattanDistance -R first-last",
+						});
+		System.out.println(res);
+	}
+	
+	private static void runKMeansWine() throws Exception {
+		SimpleKMeans km = new SimpleKMeans();
+		km.setNumClusters(7);
+		
+		String res = ClusterEvaluation.evaluateClusterer(km, 
+				new String[]{
+						"-t", "src/main/resources/wine.arff", 
+						"-c", "last",
+						"-A", "weka.core.EuclideanDistance -R first-last",
+						});
+		System.out.println(res);
+	}
+	
 }

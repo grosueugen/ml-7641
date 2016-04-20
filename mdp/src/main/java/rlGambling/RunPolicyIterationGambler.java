175613661ca4b59f743a056e11aadfaf4853a031
Eugen Grosu <grosueugen@gmail.com>
2016-04-20 23:27:57 +0200

    gambler: pi

--- mdp/src/main/java/rlGambling/RunPolicyIterationGambler.java
+++ mdp/src/main/java/rlGambling/RunPolicyIterationGambler.java
@@ -2,27 +2,27 @@ package rlGambling;
 
 import burlap.behavior.policy.Policy;
 import burlap.behavior.singleagent.EpisodeAnalysis;
-import burlap.behavior.singleagent.planning.stochastic.valueiteration.ValueIteration;
+import burlap.behavior.singleagent.planning.stochastic.policyiteration.PolicyIteration;
 import burlap.oomdp.core.Domain;
 import burlap.oomdp.core.states.State;
 import burlap.oomdp.statehashing.HashableStateFactory;
 import burlap.oomdp.statehashing.SimpleHashableStateFactory;
 
-public class RunValueIterationGambler {
+public class RunPolicyIterationGambler {
 	
 	public static void main(String[] args) {
-		GamblerDomain gamblerDomain = new GamblerDomain(70, 0.2);
+		GamblerDomain gamblerDomain = new GamblerDomain(51, 0.5);
 		Domain domain = gamblerDomain.generateDomain();
 		State initialState = gamblerDomain.createInitialState(domain);
 		GamblerReward reward = new GamblerReward();
 		GamblerTerminalState terminalState = new GamblerTerminalState();
 		HashableStateFactory hashingFactory = new SimpleHashableStateFactory();
 		
-		ValueIteration planner = new ValueIteration(domain, reward, terminalState, 1, hashingFactory, 0.1, 100);
-		planner.toggleReachabiltiyTerminalStatePruning(true);
+		PolicyIteration planner = new PolicyIteration(domain, reward, terminalState, 1, hashingFactory, 1, 100, 100);
+		
 		Policy policy = planner.planFromState(initialState);
 		EpisodeAnalysis episodeAnalysis = policy.evaluateBehavior(initialState, reward, terminalState);
-		String output = "C:\\Users\\Eugen\\Desktop\\mdp\\gambler_vi_output_" + System.currentTimeMillis();
+		String output = "C:\\Users\\Eugen\\Desktop\\mdp\\gambler_pi_output_" + System.currentTimeMillis();
 		System.out.println("writing to file: " + output);
 		episodeAnalysis.writeToFile(output);
 	}
